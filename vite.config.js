ee4c287b3173940f28e13e124a487a0df938fa9b
guibaumer <117869143+guibaumer@users.noreply.github.com>
2023-11-25 10:19:35 -0300

    configs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-sites/lista-local_storage"
+  base: "/lista-local_storage"
 })
